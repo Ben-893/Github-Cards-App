795c212ad5d2126b708ec1d7c680ff0f6c43b09d
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-07-22 13:37:32 +0100

    Added a State to Form, and handleSubmit function

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,25 @@
 import React from "react";
 
 class Form extends React.Component {
+  state = {
+    userName: "",
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.userName);
+  };
+
   render() {
     return (
-      <form action="">
-        <input type="text" placeholder="Github username"/>
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.userName}
+          onChange={(event) => this.setState({ userName: event.target.value })}
+          placeholder="Github username"
+          required
+        />
         <button>Add Card</button>
       </form>
     );
